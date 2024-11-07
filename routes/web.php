8405432e70f577faaf147b5a9bd794b0d17cc48b
root <root@radius.neo.internal>
2024-11-07 15:49:24 +0700

    dari gres

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,20 @@ use Illuminate\Support\Facades\Route;
 use App\Events\LogUpdated;
 
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('api-netpro')->group(function () {
+
+    Route::get('/', function () {
+        return response()->json(['message' => 'API Netprofe is working!']);
+    });
+
+    Route::get('/index', function () {
+       return view('index');
+    });
+
+
+    Route::get('/mikrotik-connect', [MikrotikController::class, 'connectToMikrotik']);
+    Route::get('/check-connection', [MikrotikController::class, 'checkConnection']);
+    Route::get('/login-hotspot-user', [MikrotikController::class, 'loginHotspotUser1']);
+    Route::get('/test-hotspot-login', [MikrotikController::class, 'showTestPage']);
+    Route::post('/test-hotspot-login', [MikrotikController::class, 'testHotspotLogin']);
 });
-
-Route::get('/index', function () {
-    return view('index');
-});
-
-
-Route::get('/mikrotik-connect', [MikrotikController::class, 'connectToMikrotik']);
-Route::get('/check-connection', [MikrotikController::class, 'checkConnection']);
-Route::get('/login-hotspot-user', [MikrotikController::class, 'loginHotspotUser1']);
-Route::get('/test-hotspot-login', [MikrotikController::class, 'showTestPage']);
-Route::post('/test-hotspot-login', [MikrotikController::class, 'testHotspotLogin']);
