02f2bc19f729a8692a704bf45af8aa507032d38e
Mielola <mwildab15@gmail.comsdada>
2024-11-18 14:38:45 +0700

    Deleted File

--- bootstrap/providers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\RadiusService::class,
-    App\Providers\RiakServiceProvider::class,
-];
--- resources/views/test.js
+++
@@ -1,5 +0,0 @@
-Echo.channel('data-updates')
-    .listen('DataUpdated', (e) => {
-        console.log('Data terbaru diterima:', e.data);
-        // Anda bisa memperbarui tampilan pengguna dengan data baru di sini
-    });
